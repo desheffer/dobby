3c37643eb85ff48cd0b9b5a5a8bca7a108d11a3c
Doug Sheffer <desheffer@gmail.com>
2017-06-10 00:10:44 -0400

    Switch DDNS to Cloudflare

--- roles/webserver/sync-from-dobby.sh
+++ roles/webserver/sync-from-dobby.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")/files"
 
-rsync -rv --delete --progress \
+rsync -av --delete --progress \
     --rsync-path="sudo rsync" \
     debian@192.168.0.50:/etc/letsencrypt/ etc/letsencrypt/
--- roles/webserver/sync-to-dobby.sh
+++ roles/webserver/sync-to-dobby.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")/files"
 
-rsync -rv --delete --progress \
+rsync -av --delete --progress \
     --rsync-path="sudo rsync" \
     etc/letsencrypt/ debian@192.168.0.50:/etc/letsencrypt/
