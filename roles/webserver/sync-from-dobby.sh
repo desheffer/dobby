37c7451bc3ca04f1f2a38565714f32ab5173d706
Doug Sheffer <desheffer@gmail.com>
2018-12-16 00:39:10 -0500

    Fully utilize CloudFlare

--- roles/webserver/sync-from-dobby.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/files"
-
-rsync -av --delete --progress \
-    --rsync-path="sudo rsync" \
-    pi@192.168.0.55:/etc/letsencrypt/ etc/letsencrypt/
--- roles/webserver/sync-to-dobby.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/files"
-
-rsync -av --delete --progress \
-    --rsync-path="sudo rsync" \
-    etc/letsencrypt/ pi@192.168.0.55:/etc/letsencrypt/
