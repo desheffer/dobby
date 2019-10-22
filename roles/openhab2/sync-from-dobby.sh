1958a38cc27bdb66200e5e73bac4a367a1bee8d4
Doug Sheffer <desheffer@gmail.com>
2019-10-22 01:16:36 -0400

    Upgrade all the things

--- roles/openhab2/sync-from-dobby.sh
+++ roles/openhab2/sync-from-dobby.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")/files"
 
 rsync -rv --delete --progress \
-    pi@192.168.0.50:/etc/openhab2/ etc/openhab2/
+    pi@192.168.1.50:/etc/openhab2/ etc/openhab2/
 
 echo
 git status .
